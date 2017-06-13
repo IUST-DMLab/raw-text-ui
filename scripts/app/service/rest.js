93e6addea938cc932c9583a442a8846c29790236
Majid Asgari <majid.asgari@gmail.com>
2017-06-13 20:06:01 +0430

    triples by default

--- scripts/app/service/rest.js
+++ scripts/app/service/rest.js
@@ -1,6 +1,6 @@
 app.service('RestService', ['$http', function ($http) {
-    // var baseURl = 'http://dmls.iust.ac.ir:8100';
-    var baseURl = 'http://localhost:8100';
+    var baseURl = 'http://dmls.iust.ac.ir:8100';
+    // var baseURl = 'http://localhost:8100';
 
     var self = this;
     this.ingoing = 0;
